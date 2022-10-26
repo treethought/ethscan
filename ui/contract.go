cb199c8ef5b58ced1a76ed4312e0cb7bbc36e85e
Cam Sweeney <cpsweene@gmail.com>
2022-10-26 00:34:01 +0000

    show decoded constructor args

--- ui/contract.go
+++ ui/contract.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"encoding/hex"
 	"fmt"
 
 	"code.rocketnine.space/tslocum/cbind"
@@ -98,7 +99,7 @@ func (c *ContractForm) render() {
 	grid.SetBorder(true)
 	grid.SetBorders(true)
 
-	grid.SetRows(0, 2)
+	grid.SetRows(0, 0, 0, 1)
 	grid.SetColumns(0, 0)
 
 	if c.contract == nil {
@@ -110,6 +111,7 @@ func (c *ContractForm) render() {
 	// info.SetBorder(true)
 	info.AddItem(cview.NewListItem(fmt.Sprintf("Name: %s", c.contract.ContractName)))
 	info.AddItem(cview.NewListItem(fmt.Sprintf("Compiler: %s", c.contract.CompilerVersion)))
+	info.AddItem(cview.NewListItem(fmt.Sprintf("Implementation: %s", c.contract.Implementation)))
 
 	settings := cview.NewList()
 	settings.SetTitle("Settings")
@@ -123,34 +125,57 @@ func (c *ContractForm) render() {
 
 	cabi := c.contract.ABI
 
-	abi := cview.NewList()
-	abi.SetTitle("ABI Summary")
-	abi.AddItem(cview.NewListItem(fmt.Sprint(cabi.Constructor.String())))
+	abiInfo := cview.NewList()
+	abiInfo.SetTitle("ABI Summary")
+	abiInfo.SetBorder(true)
+	abiInfo.AddItem(cview.NewListItem(fmt.Sprint(cabi.Constructor.String())))
 
 	for _, m := range c.contract.ABI.Methods {
-		abi.AddItem(cview.NewListItem(m.String()))
+		abiInfo.AddItem(cview.NewListItem(m.String()))
+	}
+
+	consArgs := cview.NewList()
+	consArgs.SetBorder(true)
+	consArgs.SetTitle("Constructor Arguments")
+
+	consArgs.AddItem(cview.NewListItem(c.contract.ConstructorArguments))
+
+	argBytes, err := hex.DecodeString(c.contract.ConstructorArguments)
+	if err != nil {
+		c.app.log.Error("failed to decode args: ", err)
+	}
+	d, err := c.contract.ABI.Constructor.Inputs.Unpack(argBytes)
+	if err != nil {
+		c.app.log.Error("failed to unpack args: ", err)
+	}
+
+	for i, d := range d {
+		arg := c.contract.ABI.Constructor.Inputs[i]
+		txt := fmt.Sprintf("[%d] %s (%s) %s", i, arg.Name, arg.Type, d)
+		consArgs.AddItem(cview.NewListItem(txt))
 	}
 
 	grid.AddItem(info, 0, 0, 1, 1, 0, 0, true)
 	grid.AddItem(settings, 0, 1, 1, 1, 0, 0, true)
-	grid.AddItem(abi, 1, 0, 2, 2, 0, 0, true)
+	grid.AddItem(consArgs, 1, 0, 1, 2, 0, 0, true)
+	grid.AddItem(abiInfo, 2, 0, 2, 2, 0, 0, true)
 
 	source := cview.NewTextView()
 	source.SetTitle("Source Code")
 	source.SetText(c.contract.SourceCode)
 	source.SetScrollable(true)
 
-	c.AddTab("overview", "Overview", grid)
-	c.AddTab("source", "Source Code", source)
+	c.tabs.AddTab("overview", "Overview", grid)
+	c.tabs.AddTab("source", "Source Code", source)
 
 	c.SetBorder(false)
 	c.SetPadding(0, 0, 0, 0)
 
 	c.SetBackgroundColor(tcell.ColorDefault)
-	c.SetTabBackgroundColor(tcell.ColorDefault)
-	c.SetTabSwitcherDivider("", " | ", "")
-	c.SetTabSwitcherAfterContent(false)
-	c.SetCurrentTab("overview")
-	c.app.app.SetFocus(c)
+	c.tabs.SetTabBackgroundColor(tcell.ColorDefault)
+	c.tabs.SetTabSwitcherDivider("", " | ", "")
+	c.tabs.SetTabSwitcherAfterContent(false)
+	c.tabs.SetCurrentTab("overview")
+	c.Frame = cview.NewFrame(c.tabs)
 
 }
